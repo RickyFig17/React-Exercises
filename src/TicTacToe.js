8b7a23f649ddce2ff3a96ea5ae47d94d716d923a
ricardo <rfigueroa2317@gmail.com>
2025-01-23 14:28:34 -0500

    wrapping up code

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,5 +1,5 @@
 import "./TicTacToe.css";
-import { use, useState } from "react";
+import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
   return (
@@ -75,32 +75,34 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
-
+    setCurrentMove(nextMove);
   }
 
-  const moves = history.map((squares, move) => {
+  const moves = history.map((move) => {
     let description;
     if (move > 0) {
-        description = 'Go to move # ' + move;
+      description = "Go to move # " + move;
     } else {
-        description = 'Go to game start';
+      description = "Go to game start";
     }
     return (
-        <li>
-            <button onClick={() => jumpTo(move)}>{description}</button>
-        </li>
-    )
-  })
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
