03726b416c16903956f8c9916efb15e2ca94388d
ricardo <rfigueroa2317@gmail.com>
2025-01-28 17:47:38 -0500

    adding basic todo list

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/tictactoe">Tic Tac Toe</Link>
-          </li>
-          <li>
-            <Link to="/thinkinginreact">Produce List</Link>
-          </li>
-          <li>
-            <Link to="/datadisplay">Profile</Link>
-          </li>
-          <li>
-            <Link to="/aboutpage">About</Link>
-          </li>
-          <li>
-            <Link to="/todolist">Todo List</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/tictactoe">Tic Tac Toe</Link>
+            </li>
+            <li>
+              <Link to="/thinkinginreact">Produce List</Link>
+            </li>
+            <li>
+              <Link to="/datadisplay">Profile</Link>
+            </li>
+            <li>
+              <Link to="/aboutpage">About</Link>
+            </li>
+            <li>
+              <Link to="/todolist">Todo List</Link>
+            </li>
+          </ul>
+        </nav>
     </>
   );
 }
